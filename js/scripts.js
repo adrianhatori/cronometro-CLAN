30be50fe60163ae361db71aa722198174ce91616
Adrian Hatori <adrianhatori@gmail.com>
2022-10-03 14:09:54 -0300

    .

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,18 @@
-let display = document.querySelector("#timer");
+let display = document.querySelector(".timerDisplay");
 let targetTime = "19:30"
+let diferençaEsperada = "0"
+let time = new Date().toLocaleTimeString();
+
+//atualiza horario
+function refreshTime() {
+    //pega horario do computador
+    time = new Date().toLocaleTimeString();
+}
 
 //Função inicia contagem regressiva
 function start() {
-
+    refreshTime();
 }
 
 //ao carregar a página a função startTimer é chamada
+window.onload = start();
